f8ba364c1fdcbaf75cd54d54d7db8ba7220a08c4
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-04-20 19:41:41 +0300

    practice js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, item) {
+    const filtered = array.filter((arg)=> arg !== item);
+    return filtered;
 };
 
 // Do not edit below this line
+
+//alternative solution
+/*
+  const removeFromArray = function (array, item) {
+     for (let i = 0; i < array.length; i++) {
+         if(array[i] == item )
+         array.splice(i,1); //remove element i, only one element must be removed
+     }
+    return array;
+    };
+
+*/
 module.exports = removeFromArray;
