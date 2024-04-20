5d6b43987994d6b766b3167e9aea74a12c63f851
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-04-20 23:50:39 +0300

    practice js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    if (year % 4 === 0) {
+        if (year % 100 !== 0 || year % 400 === 0)
+            return true;
+        else return false;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//notes:
+//leap year => divisible by 4
+//leap year must be divisible by 400 if it's divisible by 100 
+//if the year is divisible by 100 but not by 400 then it is not leap year
--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -4,22 +4,12 @@ describe('convertToCelsius', () => {
   test('works', () => {
     expect(convertToCelsius(32)).toEqual(0);
   });
-  test.skip('rounds to 1 decimal', () => {
-    expect(convertToCelsius(100)).toEqual(37.8);
-  });
-  test.skip('works with negatives', () => {
-    expect(convertToCelsius(-100)).toEqual(-73.3);
-  });
+  test
+  test
 });
 
 describe('convertToFahrenheit', () => {
-  test.skip('works', () => {
-    expect(convertToFahrenheit(0)).toEqual(32);
-  });
-  test.skip('rounds to 1 decimal', () => {
-    expect(convertToFahrenheit(73.2)).toEqual(163.8);
-  });
-  test.skip('works with negatives', () => {
-    expect(convertToFahrenheit(-10)).toEqual(14);
-  });
+  test
+  test
+  test
 });
