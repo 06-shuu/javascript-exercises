26bea15f67ba4353feb37344caa2c55ae4be983c
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-06-24 08:58:02 +0300

    complete palindromes ex.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    /**
+     * 1. delete spaces and punctuations
+     * 2. reverse the string
+     * 3. check the equality between the original string and the reversed one
+     */
+
+    const clearString = str.replace(/[^a-zA-Z0-9]/g, '')
+        .toLowerCase()
+    const reversedString = clearString.split('').reverse().join('');
+    return clearString === reversedString
 
 };
 
+console.log(palindromes("A car, a man, a maraca"))
 // Do not edit below this line
 module.exports = palindromes;
