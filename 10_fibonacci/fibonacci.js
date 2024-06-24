890c547775a74a9316b48a5aab7f9fb1031e771f
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-06-24 09:24:22 +0300

    complete fib. ex.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    //checks if the arg is a number or note them apply fib 
+    //fn = fn-1 + fn-2
+
+    let num;
+    if (typeof n !== "number") num = parseInt(n)
+    else num = n
+
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+
+    let first = 0;
+    let second = 1;
+
+
+    for (let i = 1; i <= num; i++) {
+        nextTerm = first + second;
+        first = second;
+        second = nextTerm;
+    }
+
+    return first
 
 };
+
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
