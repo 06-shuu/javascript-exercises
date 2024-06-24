9629f760efa4d347f0335db872ac340e4d46de8a
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-06-24 08:32:40 +0300

    complete calculator ex.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,52 @@
-const add = function() {
-	
+//1
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+console.log(add(1, 2))
+
+//2
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+console.log(subtract(2, 1))
+
+//3
+
+const nums = [1, 2, 3, 4]
+
+const sum = function (arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0)
 };
 
-const multiply = function() {
+console.log(sum(nums))
 
+//4
+const multiply = function (arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1)
 };
 
-const power = function() {
-	
+console.log(multiply(nums))
+
+//5
+const power = function (a, b) {
+  return Math.pow(a, b)
+};
+console.log(power(2, 3))
+
+//6
+const factorial = function (n) {
+  if (n < 0)
+    return -1;
+  else if (n == 0)
+    return 1;
+  else {
+    return (n * factorial(n - 1));
+  }
 };
 
-const factorial = function() {
-	
-};
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
